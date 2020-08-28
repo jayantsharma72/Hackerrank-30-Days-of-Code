ca7a1cb832d9ba6ebe0bb82788a77a5fff0b5223
jayantjayant11 <jayantjayant11@gmail.com>
2020-08-28 08:53:50 +0530

    Eight Commit:Day 8 code

---
+++ Day_7.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        StringBuilder stb = new StringBuilder();
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        for(int i=arr.length-1;i>=0;i--)
+        {
+            if(i==0)
+                stb.append(arr[i]);
+            else
+                stb.append(arr[i]).append(" ");
+        }
+        System.out.print(stb);
+        scanner.close();
+    }
+}
